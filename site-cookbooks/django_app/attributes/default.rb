8fc0c91070e8a6c6bb7d434cf14f40f277c31773
manojlds <manojlds@gmail.com>
2013-06-25 16:26:32 +0530

    Extracting out attributes

---
+++ site-cookbooks/django_app/attributes/default.rb
@@ -0,0 +1,4 @@
+default["django_app"]["name"] = "pari"
+default["django_app"]["settings_module"] = "prod" 
+default["django_app"]["source_path"] = "/pari" 
+default["django_app"]["server_name"] = "ruralindiaonline.org www.ruralindiaonline.org"
--- site-cookbooks/django_app/recipes/default.rb
+++ site-cookbooks/django_app/recipes/default.rb
@@ -1,15 +1,6 @@
-#
-# Cookbook Name:: django_app
-# Recipe:: default
-#
-# Copyright 2013, YOUR_COMPANY_NAME
-#
-# All rights reserved - Do Not Redistribute
-#
-
 npm_package "less"
 
-# python_virtualenv "/.venv/pari" do
+# python_virtualenv "/.venv/#{node["django_app"]["name"]}" do
 #   owner "root"
 #   group "root"
 #   action :create
@@ -17,11 +8,11 @@ npm_package "less"
 # end
 
 # bash "install_app" do
-#   cwd "/pari"
+#   cwd node["django_app"]["source_path"]
 #   code <<-EOH 
-#     source /.venv/pari/bin/activate
-#     export DJANGO_SETTINGS_MODULE=pari.settings.prod
-#     pip install -r requirements/prod.txt
+#     source /.venv/#{node["django_app"]["name"]}/bin/activate
+#     export DJANGO_SETTINGS_MODULE=#{node["django_app"]["name"]}.settings.#{node["django_app"]["settings_module"]}
+#     pip install -r requirements/#{node["django_app"]["settings_module"]}.txt
 #     python manage.py syncdb --noinput
 #     python manage.py migrate
 #     python manage.py collectstatic --noinput
@@ -29,25 +20,30 @@ npm_package "less"
 #   action :run
 # end
 
-gunicorn_config "/etc/gunicorn/pari.py" do
+gunicorn_config "/etc/gunicorn/#{node["django_app"]["name"]}.py" do
   action :create
   pid "gunicorn.pid"
 end
 
-supervisor_service "gunicorn_pari" do
+supervisor_service "gunicorn_#{node["django_app"]["name"]}" do
   action [:enable, :start]
   autostart true
   autorestart true
   redirect_stderr true
-  environment :DJANGO_DEBUG => "true", :DJANGO_SETTINGS_MODULE => "pari.settings.prod", :PATH => "/.venv/pari/bin:/usr/local/bin:/bin:/usr/bin:/usr/local/sbin:/usr/sbin:/sbin:/home/vagrant/bin:"
-  command "/.venv/pari/bin/gunicorn pari.wsgi:application -c pari.py -p gunicorn.pid --error-logfile error.log --log-file log.log"
-  directory "/pari"
+  environment :DJANGO_SETTINGS_MODULE => "#{node["django_app"]["name"]}.settings.#{node["django_app"]["settings_module"]}", :PATH => "/.venv/#{node["django_app"]["name"]}/bin:/usr/local/bin:/bin:/usr/bin:/usr/local/sbin:/usr/sbin:/sbin:/home/vagrant/bin:"
+  command "/.venv/#{node["django_app"]["name"]}/bin/gunicorn #{node["django_app"]["name"]}.wsgi:application -c #{node["django_app"]["name"]}.py -p gunicorn.pid --error-logfile error.log --log-file log.log"
+  directory "/#{node["django_app"]["source_path"]}"
   user "root"
-  process_name "gunicorn_pari"
+  process_name "gunicorn_#{node["django_app"]["name"]}"
 end
 
-template "/etc/nginx/sites-enabled/pari" do
+template "/etc/nginx/sites-enabled/#{node["django_app"]["name"]}" do
     source "nginx.django.conf.erb"
+    variables({
+      :name => node["django_app"]["name"],
+      :server_name => node["django_app"]["server_name"],
+      :source_path => node["django_app"]["source_path"],
+    })
     owner "root"
     group "root"
     mode "644"
