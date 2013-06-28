0ffe0bcec07e301b1fc1b3859e30595c1f07a55d
manojlds <manojlds@gmail.com>
2013-06-28 13:01:22 +0530

    User supervisor logging

--- site-cookbooks/django_app/recipes/default.rb
+++ site-cookbooks/django_app/recipes/default.rb
@@ -34,7 +34,7 @@ supervisor_service "gunicorn_#{node["django_app"]["name"]}" do
   autorestart true
   redirect_stderr true
   environment :DJANGO_SETTINGS_MODULE => "#{node["django_app"]["name"]}.settings.#{node["django_app"]["settings_module"]}", :PATH => "/.venv/#{node["django_app"]["name"]}/bin:/usr/local/bin:/bin:/usr/bin:/usr/local/sbin:/usr/sbin:/sbin:/home/vagrant/bin:"
-  command "/.venv/#{node["django_app"]["name"]}/bin/gunicorn #{node["django_app"]["name"]}.wsgi:application -c #{node["django_app"]["name"]}.py -p gunicorn.pid --error-logfile error.log --log-file log.log"
+  command "/.venv/#{node["django_app"]["name"]}/bin/gunicorn #{node["django_app"]["name"]}.wsgi:application -c #{node["django_app"]["name"]}.py -p gunicorn.pid"
   directory node["django_app"]["source_path"]
   user "root"
   process_name "gunicorn_#{node["django_app"]["name"]}"
