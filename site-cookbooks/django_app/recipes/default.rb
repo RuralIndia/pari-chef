bf2b8cf03002efd9985f0354cdc3cf56ff789d73
manojlds <manojlds@gmail.com>
2013-09-10 11:59:40 +0530

    Moving sensitive data to data bags

--- site-cookbooks/django_app/recipes/default.rb
+++ site-cookbooks/django_app/recipes/default.rb
@@ -1,3 +1,8 @@
+mandrill_api_key = data_bag_item('api_keys', 'mandrill_api_key')['key']
+recaptcha_private_key = data_bag_item('api_keys', 'recaptcha')['private_key']
+recaptcha_public_key = data_bag_item('api_keys', 'recaptcha')['public_key']
+secret_key = data_bag_item('api_keys', 'secret_key')['key']
+
 # hostsfile_entry '127.0.0.1' do
 #   hostname  node["django_app"]["server_name"]
 # end
@@ -30,6 +35,12 @@ python_virtualenv "/.venv/#{node["django_app"]["name"]}" do
   action :create
 end
 
+git node["django_app"]["source_path"] do
+  repository "git://github.com/RuralIndia/pari.git"
+  reference "master"
+  action :checkout
+end
+
 # bash "install_app" do
 #   cwd node["django_app"]["source_path"]
 #   code <<-EOH 
@@ -55,13 +66,13 @@ supervisor_service "gunicorn_#{node["django_app"]["name"]}" do
   startretries 10
   redirect_stderr true
   environment :DJANGO_DEBUG => node["django_app"]["debug"],
-              :DJANGO_SECRET_KEY => node["django_app"]["secret_key"],
+              :DJANGO_SECRET_KEY => secret_key,
               :DJANGO_DB_PASSWORD => node["django_app"]["db_password"],
               :DJANGO_DB_USER => node["django_app"]["db_user"],
               :DJANGO_DB_NAME => node["django_app"]["db_name"],
-              :DJANGO_MANDRILL_API_KEY => node["django_app"]["mandrill_api_key"],
-              :DJANGO_RECAPTCHA_PRIVATE_KEY => node["django_app"]["recaptcha_private_key"],
-              :DJANGO_RECAPTCHA_PUBLIC_KEY => node["django_app"]["recaptcha_public_key"],
+              :DJANGO_MANDRILL_API_KEY => mandrill_api_key,
+              :DJANGO_RECAPTCHA_PRIVATE_KEY => recaptcha_private_key,
+              :DJANGO_RECAPTCHA_PUBLIC_KEY => recaptcha_public_key,
               :DJANGO_SETTINGS_MODULE => "#{node["django_app"]["name"]}.settings.#{node["django_app"]["settings_module"]}", 
               :PATH => "/.venv/#{node["django_app"]["name"]}/bin:/usr/local/bin:/bin:/usr/bin:/usr/local/sbin:/usr/sbin:/sbin:/home/vagrant/bin:"
   command "/.venv/#{node["django_app"]["name"]}/bin/gunicorn #{node["django_app"]["name"]}.wsgi:application -c #{node["django_app"]["name"]}.py -p gunicorn.pid -u www-data -g www-data"
